c5e74d90fcc40182682afbdee6fca5851c9326f6
sawa <121893056+Sawachanamandakith@users.noreply.github.com>
2025-04-16 12:55:13 +0530

    first commit

---
+++ app/src/main/java/com/example/test/activity_login1.java
@@ -0,0 +1,16 @@
+package com.example.test;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_login1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login1);
+    }
+}
